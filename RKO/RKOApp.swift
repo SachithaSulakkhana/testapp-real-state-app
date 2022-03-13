8dd6ca305f7c308682a61a0b6458b23f52125415
sachitha sulakkhana <sachithasulakkhana@gmail.com>
2022-03-13 11:55:32 +0530

    Initial Commit

---
+++ RKO/RKOApp.swift
@@ -0,0 +1,17 @@
+//
+//  RKOApp.swift
+//  RKO
+//
+//  Created by Sachitha Sulakkhana on 2022-03-13.
+//
+
+import SwiftUI
+
+@main
+struct RKOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
